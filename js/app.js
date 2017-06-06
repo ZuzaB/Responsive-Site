6eb49f3e635b628d4d0f427f4db94d5997f39dab
ZuzaB <lacznaz@wp.pl>
2017-06-06 17:23:26 +0200

    add script to chanege class in menu during scrolling

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 $(function(){ 
     var nav = document.querySelector('nav');
     var lisMenue = nav.querySelectorAll('a');
-    console.log(lisMenue);
     
     for(i=0; i < lisMenue.length; i++){
         lisMenue[i].addEventListener('click', function(){
             for(i=0; i < lisMenue.length; i++){
               lisMenue[i].classList.remove('active'); 
             }
-            this.classList.add('active');  
-            
+            this.classList.add('active');   
         });
     }
+    
+    var sections = $('.section');
+    var menu = $('div.top');
+    var menu_height = menu.outerHeight();
+    
+  
+    $(window).on('scroll', function () {
+      var cur_pos = $(this).scrollTop();
+        
+      sections.each(function() {
+        var top = $(this).offset().top - menu_height;
+        var bottom = top + $(this).outerHeight();
+    
+        if (cur_pos >= top && cur_pos <= bottom) {
+            menu.find('a').removeClass('active');
+            sections.removeClass('active');
+      
+          $(this).addClass('active');
+          menu.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+       }
+    });
+    });
 });
