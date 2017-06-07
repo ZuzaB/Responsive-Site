a9eaca8fef1770b65ffd08496df9edb2e2514632
ZuzaB <lacznaz@wp.pl>
2017-06-07 17:31:01 +0200

    add script to mobile sticky menu

--- js/app.js
+++ js/app.js
@@ -11,25 +11,47 @@ $(function(){
         });
     }
     
-    var sections = $('.section');
-    var menu = $('div.top');
-    var menu_height = menu.outerHeight();
+    var topElement = document.querySelector('.top');
+    var topElement_height = getComputedStyle(topElement).height.split('px')[0];
+    var nav_height = getComputedStyle(nav).height.split('px')[0];
+    var mobile = window.matchMedia("screen and (max-width: 540px)");
+        
+     function stickyScroll(e){
+        if( window.pageYOffset > (topElement_height - nav_height) / 2 && mobile.matches) {
+            nav.classList.add('mobile-menu');
+        }
+
+        if( window.pageYOffset < (topElement_height - nav_height) / 2 && mobile.matches) {
+            nav.classList.remove('mobile-menu');
+        }   
+    }
+    window.addEventListener('scroll', stickyScroll);
+    mobile.addListener(function(mobile){//może wprowadzić zmiany?
+         if(mobile.matches){ 
+             window.addEventListener('scroll', stickyScroll);
+        }else{
+            nav.classList.remove('mobile-menu');
+        }
+    });
     
+    var $sections = $('.section'); //przerobić na Vanilla js?
+    var $menu = $('div.top');
+    var menu_height = $menu.outerHeight();
   
     $(window).on('scroll', function () {
       var cur_pos = $(this).scrollTop();
         
-      sections.each(function() {
+      $sections.each(function() {
         var top = $(this).offset().top - menu_height;
         var bottom = top + $(this).outerHeight();
     
         if (cur_pos >= top && cur_pos <= bottom) {
-            menu.find('a').removeClass('active');
-            sections.removeClass('active');
+            $menu.find('a').removeClass('active');
+            $sections.removeClass('active');
       
           $(this).addClass('active');
-          menu.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+          $menu.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
        }
-    });
+      });
     });
 });
