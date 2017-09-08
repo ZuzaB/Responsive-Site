a47f2657be4f4efb3f2d0e2d5029aa9a5a4c317d
ZuzaB <lacznaz@wp.pl>
2017-09-08 17:41:25 +0200

    add some changes

--- js/app.js
+++ js/app.js
@@ -1,55 +1,55 @@
-$(function(){ 
+$(function(){
     var nav = document.querySelector('nav');
     var lisMenue = nav.querySelectorAll('a');
-    
-    for(i=0; i < lisMenue.length; i++){
-        lisMenue[i].addEventListener('click', function(){
-            for(i=0; i < lisMenue.length; i++){
-              lisMenue[i].classList.remove('active'); 
+
+    for(var i=0; i < lisMenue.length; i++){
+        lisMenue[i].addEventListener('click', function(e){
+            for(var i=0; i < lisMenue.length; i++){
+              lisMenue[i].classList.remove('active');
             }
-            this.classList.add('active');   
+            this.classList.add('active');
         });
     }
 
     var topElement = document.querySelector('.top');
     var mobile = window.matchMedia("screen and (max-width: 540px)");
-        
+
      function stickyScroll(e){
         var topElement_height = getComputedStyle(topElement).height.split('px')[0];
         var nav_height = getComputedStyle(nav).height.split('px')[0];
-         
+
         if( window.pageYOffset > (topElement_height - nav_height) / 2 && mobile.matches) {
             nav.classList.add('mobile-menu');
         }
 
         if( window.pageYOffset < (topElement_height - nav_height) / 2 && mobile.matches) {
             nav.classList.remove('mobile-menu');
-        }   
+        }
     }
     window.addEventListener('scroll', stickyScroll);
     mobile.addListener(function(mobile){
-         if(mobile.matches){ 
+         if(mobile.matches){
              window.addEventListener('scroll', stickyScroll);
         }else{
             nav.classList.remove('mobile-menu');
         }
     });
-    
-    var $sections = $('.section'); //przerobić na Vanilla js?
+
+    var $sections = $('.section');
     var $menu = $('div.top');
     var menu_height = $menu.outerHeight();
-  
+
     $(window).on('scroll', function () {
       var cur_pos = $(this).scrollTop();
-        
+
       $sections.each(function() {
         var top = $(this).offset().top - menu_height;
         var bottom = top + $(this).outerHeight();
-    
+
         if (cur_pos >= top && cur_pos <= bottom) {
             $menu.find('a').removeClass('active');
             $sections.removeClass('active');
-      
+
           $(this).addClass('active');
           $menu.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
        }
